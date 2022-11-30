0d824d8d271d00b4f7594937a86f753ac3932fe4
2010person <116949561+2010person@users.noreply.github.com>
2022-11-30 18:43:57 +0000

    Added an index.php
    This will be the home page of the website it is coded in PHP and HTML.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en"
+<head>
+    <meta charset="UTF-8"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Aahan in Space!</title>
+</head>
+<body>
+        <?php echo "Hello welcome to the webpage of Aahan in Space, this website is constantly improving so sorry for the mess. Are you having any issues if so please can you report them on the channel discord server or on the GitHub repo page for this website." ;
+        ?>
+</body>
+</html>
