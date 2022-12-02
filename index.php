e66a1c9604c7777e3ecd3ce9c5efa2030d7e583f
2010person <116949561+2010person@users.noreply.github.com>
2022-12-02 20:04:39 +0000

    Updated index.php
    Small error in HTML code.

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
     <strong>Hello welcome to the webpage of Aahan in Space.</strong>
     <p>Are you having any issues if so please can you report them on the <a href="https://discord.com/channels/1031920032084672573/1036228918119772212"</a>Discord server</p>
     <p>Or on the <a href="https://github.com/2010person/aahaninspace-website"</a>GitHub repo page for this website</p>
-    <p>Find out more about Aahan in Space <a href="https://aahaninspace.tk/about" here</a></p>
+    <p>Find out more about Aahan in Space <a href="https://aahaninspace.tk/about"</a>here</p>
 </body>
 </html>
