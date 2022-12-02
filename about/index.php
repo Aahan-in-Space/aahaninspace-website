ea8970ab41ea0035591045b560d4722a2dacb2e0
2010person <116949561+2010person@users.noreply.github.com>
2022-12-02 20:17:16 +0000

    More changes
    Resolved another error in aahaninspace.tk/about

--- about/index.php
+++ about/index.php
@@ -10,7 +10,7 @@
     <p>I am a YouTuber who covers astronomy through engaging videos. We talk about various topics like the Moon or Mars.</p>
     <p>This is the link to my <a href="https://youtube.com/@aahaninspace">YouTube channel!!</a></p>
     <p>I also have a Discord server, where we talk about cool things and recieve weekly updates in various topics!!</p>
-    <p>Here you can join my<a href="https://discord.com/channels/1031920032084672573/1036228918119772212"Discord server!!</a><p/>
+    <p>Here you can join my<a href="https://discord.com/channels/1031920032084672573/1036228918119772212"</a>Discord server!!<p/>
     <p>Thank you for reading this about me section, hope you enjoy!!</p>
     <p>Go to <a href="https://www.aahaninspace.tk">HOME</a></p>
 </body>
