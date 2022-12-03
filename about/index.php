30b6bd9dcc15c8783308a3a3d583ab628b405053
2010person <116949561+2010person@users.noreply.github.com>
2022-12-03 21:49:33 +0000

    Updated index.php
    Hope this has resolved the error.

--- about/index.php
+++ about/index.php
@@ -10,8 +10,8 @@
     <p>I am a YouTuber who covers astronomy through engaging videos. We talk about various topics like the Moon or Mars.</p>
     <p>This is the link to my <a href="https://youtube.com/@aahaninspace">YouTube channel!!</a></p>
     <p>I also have a Discord server, where we talk about cool things and recieve weekly updates in various topics!!</p>
-    <p>Here you can join my<a href="https://discord.com/channels/1031920032084672573/1036228918119772212"</a>Discord server!!<p/><br>
-    <p>Thank you for reading this about me section, hope you enjoy!!</p>
+    <p>Here you can join my<a href="https://discord.com/channels/1031920032084672573/1036228918119772212"</a> Discord server!!</p>
+    <p>Thank you for reading this about me section, hope you enjoy my content!!</p>
     <p><a href="https://www.aahaninspace.tk">Go to HOME</a></p>
 </body>
 </html>
