db3293d2c9953e2dda7318b3def90ec4750822a6
2010person <116949561+2010person@users.noreply.github.com>
2022-12-08 18:13:57 +0000

    Updated index.php
    More Google Ads views.

--- ads/index.php
+++ ads/index.php
@@ -9,4 +9,16 @@
 </head>
 <body>
     <strong>Thank you for volunteering with ads!!!</strong>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4531701733540995"
+     crossorigin="anonymous"></script>
+<!-- addefault -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4531701733540995"
+     data-ad-slot="1091167966"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 </body>
