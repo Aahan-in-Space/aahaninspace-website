4aace58254c7fa998a51d7a54834b9099400e433
2010person <116949561+2010person@users.noreply.github.com>
2022-12-08 18:01:23 +0000

    Added the ad link
    Setting up optional link for Google Ads.

---
+++ ads/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en"
+<head>
+    <meta charset="UTF-8"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank you!!!</title>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4531701733540995"
+     crossorigin="anonymous"></script>
+</head>
+<body>
+    <strong>Thank you for volunteering with ads!!!</strong>
+</body>
