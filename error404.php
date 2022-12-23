eb2d7fc8b9a5d3a83df5a5fa7f4f12134916dbe7
2010person <116949561+2010person@users.noreply.github.com>
2022-12-23 11:36:52 +0000

    Added error 404 page
    I have added an error 404 page to resolve the GitHub Issue.

---
+++ error404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Error 404 not found</title>
+</head>
+<body>
+    <h1>Error 404 - webpage not found</h1>
+    <h2>Whoops! Look like we haven't found the planet you're on!!</h2>
+    <a href="https://www.aahaninspace.tk">Return to HOME</a>
+</body>
+</html>
