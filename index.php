d8c06baeeefa487e135c149f78cd8c8a0fa053b1
2010person <116949561+2010person@users.noreply.github.com>
2022-12-25 11:25:43 +0000

    Updated index.php
    Added information about signing up for the Mars Sample Return!

--- index.php
+++ index.php
@@ -10,5 +10,8 @@
     <p><a href="https://discord.com/channels/1031920032084672573/1036228918119772212"</a>Are you having any issues if so please can you report them on the Discord server</p>
     <p><a href="https://github.com/2010person/aahaninspace-website"</a>Or on the GitHub repo page for this website</p>
     <p><a href="https://aahaninspace.tk/about"</a>Find out more about Aahan in Space here</p>
+    <p><a href="https://mars.nasa.gov/participate/send-your-name/"</a> Click this link to sign up for the Mars Sample Return mission.</p>
+    <p>See mine below!!!</p>
+    <iframe width="980" height="410" src="https://mars.nasa.gov/layout/embed/send-your-name/future/certificate/?cn=427679920775" frameborder="0"></iframe>
 </body>
 </html>
