3981f4418e91e7a2d18dc577a567da8f02d68f99
2010person <116949561+2010person@users.noreply.github.com>
2022-12-31 19:53:35 +0000

    Updated index.php
    Added the link to ukrainewar page.

--- index.php
+++ index.php
@@ -15,5 +15,6 @@
     <iframe width="980" height="410" src="https://mars.nasa.gov/layout/embed/send-your-name/future/certificate/?cn=427679920775" frameborder="0"></iframe>
     <p><a href="https://discord.com/channels/1031920032084672573/1036228918119772212">Enjoying, why not join the Dicord server, see the widget below.</p>
     <iframe src="https://discord.com/widget?id=1031920032084672573&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    <a href="/ukrainewar">Please click here to read about Aahan in Space's current stance on the war in Ukraine and its affect on users within the Russian Federation.</a>
 </body>
 </html>
